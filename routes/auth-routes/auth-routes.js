b85cb79f73ddc60a6ea402c0e2561bad767fb118
killianmh <killianmh@gmail.com>
2018-07-26 15:57:01 -0700

    fixed routing and installed jsonwebtoken

---
+++ routes/auth-routes/auth-routes.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../../Models');
+var passport = require('passport');
+// require('../../config/passport')(passport);
+// var bcrypt = require('bcrypt-nodejs');
+var jwt = require('jsonwebtoken');
+const Op = require('sequelize').Op;
+var Sequelize = require('sequelize');
+
+
+module.exports = router;
