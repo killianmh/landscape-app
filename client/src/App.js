6d54a4f86701e25761c76ef97e9fe1deea15ce0a
killianmh <killianmh@gmail.com>
2018-08-01 14:17:37 -0700

    make sure api calls work

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    axios.get('/api/test').then((res) => {
+      console.log(res.data);
+    })
+  }
+
   render() {
     return (
       <div className="App">
