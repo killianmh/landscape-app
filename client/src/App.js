eec186db1b7e67b11023c0b81a0982ef497ea791
killianmh <killianmh@gmail.com>
2018-09-10 18:00:42 -0700

    landing page hero styled

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
 
+import leaf from "./assets/images/leaf.svg";
+
 class App extends Component {
   componentDidMount() {
     axios.get('/api/test').then((res) => {
@@ -12,11 +14,36 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col"></div>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="#">
+            <img src={leaf} width="30" height="30" alt=""></img>
+          </a>
+        </nav>
+        <div className="jumbotron jumbotron-fluid jumbo-background">
+          <div className="container">
+            <div className="row align-items-center landing-title">
+              <div className="col text-center">
+                <div>Landscapr</div>
+                
+              </div>
+            </div>
+            <div className="row">
+              <div>
+              <button>button</button>
+              </div>
+            </div>
+        </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              test
+            </div>
+          </div>
         </div>
       </div>
+      
     );
   }
 }
